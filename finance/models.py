d8bb4783e24a4b713e3a79d8a4fcd9f0ffac6b74
Anton <Grygorenko23@email.com>
2026-08-05 14:25:55 +0200

    Add payment model

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    class PaymentType(models.TextChoices):
+        INCOME = "income"
+        EXPENSE = "expense"
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_type = models.CharField(max_length=7, choices=PaymentType.choices)
+    date = models.DateField()
+    category = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
