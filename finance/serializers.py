ed76b2d36b9dcd2d39cf3129af25c4faa5edd206
Anton <Grygorenko23@email.com>
2026-08-06 15:33:08 +0200

    "Add payments API"

---
+++ finance/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from finance.models import Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "amount",
+            "payment_type",
+            "date",
+            "category",
+            "description",
+            "created_at",
+        )
+        read_only_fields = ("id", "created_at")
